be6aa355f87ce4ae53b2419eb09146cf9823ed52
Yashvardhan Singh <78314979+YashSingh667@users.noreply.github.com>
2023-03-20 22:57:44 +0530

    First commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#to run your flask server. Keep your port number for flask as 5025 .
+# The run.sh file provided by you in home folder should start your flask server on localhost(127.0.0.1) on port (5025)
+# ( Note that front end etc. need not be flask).
---
+++ setup_front_end.sh
@@ -0,0 +1,1 @@
+# install frontend dependencies
