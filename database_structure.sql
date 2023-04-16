3ace44b44cc27fb7dc8d32ef9d4c64b981d5c8d7
Rashmithapittala <rashmithapittala21@gmail.com>
2023-04-17 03:06:35 +0530

    final tables?

--- database_structure.sql
+++ database_structure.sql
@@ -17,16 +17,17 @@ CREATE TABLE Broker_details (
 );
 
 
-DROP TABLE IF EXISTS User;
-CREATE TABLE User ( 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users ( 
     customerID varchar(50) NOT NULL,
     u_password varchar(50) NOT NULL,
-    broker_id varchar(50) NOT NULL,
-    username varchar(50) NOT NULL,
+    brokerID varchar(50) NOT NULL,
+    usersname varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
     dob DATE NOT NULL,
     fullname varchar(50) NOT NULL,
     PRIMARY KEY (customerID),
-    CONSTRAINT fk_brokerID_User
+    CONSTRAINT fk_brokerID_users
     FOREIGN KEY (brokerID)
     REFERENCES Broker_details(brokerID)
 
@@ -35,22 +36,11 @@ CREATE TABLE User (
 DROP TABLE IF EXISTS BrokerT;
 CREATE TABLE BrokerT ( 
     brokerID varchar(50) NOT NULL,
-    exchangebrokerID varchar(50) DEFAULT NULL,
+    exchangebrokerID varchar(50) NOT NULL,
     PRIMARY KEY (exchangebrokerID),
     CONSTRAINT fk_brokerID_BrokerT
     FOREIGN KEY (brokerID)
     REFERENCES Broker_details(brokerID)
-
-);
-
-DROP TABLE IF EXISTS stockexchange;
-CREATE TABLE stockexchange (
-    exchangename varchar(50) NOT NULL,
-    exchangebrokerID varchar(50) NOT NULL,
-    PRIMARY KEY(exchangebrokerID),
-    CONSTRAINT fk_exchangebrokerID_stockexchange
-    FOREIGN KEY (exchangebrokerID)
-    REFERENCES BrokerT
 
 );
 
@@ -61,16 +51,76 @@ CREATE TABLE company (
     cdate DATE NOT NULL,
     highprice FLOAT DEFAULT NULL,
     lowprice FLOAT DEFAULT NULL,
-    PRIMARY KEY (companyID, exchangename, cdate),
-    CONSTRAINT fk_exchangename_company
-    FOREIGN KEY (exchangename)
-    REFERENCES stockexchange
+    PRIMARY KEY (companyID, exchangename, cdate)
 );
+
+
+
+DROP TABLE IF EXISTS stockexchange;
+CREATE TABLE stockexchange (
+    exchangename varchar(50) NOT NULL,
+    exchangebrokerID varchar(50) NOT NULL,
+    PRIMARY KEY(exchangebrokerID),
+    CONSTRAINT fk_exchangebrokerID_stockexchange
+    FOREIGN KEY (exchangebrokerID)
+    REFERENCES BrokerT(exchangebrokerID)
+    -- CONSTRAINT fk_exchangename_company
+    -- FOREIGN KEY (exchangename)
+    -- REFERENCES company(exchangename)
+
+);
+
+
+
+
+
+
 
 DROP TABLE IF EXISTS Orderbook;
 CREATE TABLE Orderbook (
     transactionID varchar(50) NOT NULL,
+    companyID varchar(50) NOT NULL,
+    customerID varchar(50) NOT NULL,
+    exchangebrokerID varchar(50) NOT NULL,
+    stockvolume int NOT NULL,
+    stockprice FLOAT NOT NULL,
+    PRIMARY KEY (transactionID),
+    CONSTRAINT fk_transactionID_orderbook
+    FOREIGN KEY (transactionID)
+    REFERENCES Transactions(transactionID),
+    -- CONSTRAINT fk_companyID_orderbook
+    -- FOREIGN KEY (companyID)
+    -- REFERENCES company(companyID),
+    CONSTRAINT fk_customerID_orderbook
+    FOREIGN KEY (customerID)
+    REFERENCES users(customerID),
+    CONSTRAINT fk_exchangebrokerID_orderbook
+    FOREIGN KEY (exchangebrokerID)
+    REFERENCES BrokerT(exchangebrokerID)
 
 );
+
+
+DROP TABLE IF EXISTS Portfolio;
+CREATE TABLE Portfolio (
+    stockvolume int NOT NULL,
+    exchangebrokerID varchar(50) NOT NULL,
+    companyID varchar(50) NOT NULL,
+    customerID varchar(50) NOT NULL,
+    PRIMARY KEY(exchangebrokerID, companyID, customerID),
+    -- CONSTRAINT fk_companyID_Portfolio
+    -- FOREIGN KEY (companyID)
+    -- REFERENCES company(companyID),
+    CONSTRAINT fk_customerID_Portfolio
+    FOREIGN KEY (customerID)
+    REFERENCES users(customerID),
+    CONSTRAINT fk_exchangebrokerID_Portfolio
+    FOREIGN KEY (exchangebrokerID)
+    REFERENCES BrokerT(exchangebrokerID)
+
+);
+
+END TRANSACTION;
+
 
 
