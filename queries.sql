30cd505ef7c98fe8bb87f972373483cc335feae8
joinakd <akdwebmails@gmail.com>
2023-04-27 17:00:55 +0530

    updated buying query

--- queries.sql
+++ queries.sql
@@ -25,31 +25,52 @@ WHERE customerID = /var;
 
 --- Buying of stock ---
 
+
 BEGIN TRANSACTION;
 
 -- Check if user exists
-SELECT * FROM users WHERE username = '<username>';
+IF EXISTS (SELECT * FROM users WHERE username = '<username>') THEN
+    -- Check user's wallet balance
+    SELECT wallet FROM users WHERE username = '<username>';
 
--- Check user's wallet balance
-SELECT wallet FROM users WHERE username = '<username>';
+    -- Check if user has sufficient balance
+    IF ((SELECT wallet FROM users WHERE username = '<username>') >= (<stock_volume> * <stock_price>)) THEN
+        -- Check if user already has the stock in their portfolio
+        IF EXISTS (SELECT * FROM Portfolio WHERE customerID = <customer_id> AND Stock_name = '<stock_name>') THEN
+            -- Update user's portfolio stock volume
+            UPDATE Portfolio SET stockvolume = stockvolume + <stock_volume>
+            WHERE customerID = <customer_id> AND Stock_name = '<stock_name>';
+        ELSE
+            -- Add new stock to user's portfolio
+            INSERT INTO Portfolio (stockvolume, exchangebrokerID, Stock_name, customerID)
+            VALUES (<stock_volume>, '<exchange_broker_id>', '<stock_name>', <customer_id>);
+        END IF;
 
--- Check if user has sufficient balance
-IF ((SELECT wallet FROM users WHERE username = '<username>') >= (<stock_volume> * <stock_price>)) THEN
-    -- Buy stock from company table
-    INSERT INTO Orderbook (Stock_name, customerID, exchangebrokerID, stockvolume, stockprice, t_date)
-    VALUES ('<stock_name>', <customer_id>, '<exchange_broker_id>', <stock_volume>, <stock_price>, '<transaction_date>');
+        -- Buy stock from company table
+        INSERT INTO Orderbook (Stock_name, customerID, exchangebrokerID, stockvolume, stockprice, t_date)
+        VALUES ('<stock_name>', <customer_id>, '<exchange_broker_id>', <stock_volume>, <stock_price>, '<transaction_date>');
 
-    -- Update user's wallet balance
-    UPDATE users SET wallet = wallet - (<stock_volume> * <stock_price>) WHERE username = '<username>';
+        -- Update user's wallet balancei
+        UPDATE users SET wallet = wallet - (<stock_volume> * <stock_price>) WHERE username = '<username>';
 
-    -- Update user's portfolio
-    INSERT INTO Portfolio (stockvolume, exchangebrokerID, Stock_name, customerID)
-    VALUES (<stock_volume>, '<exchange_broker_id>', '<stock_name>', <customer_id>)
-    ON DUPLICATE KEY UPDATE stockvolume = stockvolume + <stock_volume>;
-
+    ELSE
+        -- Throw an error if user has insufficient balance
+        RAISE EXCEPTION 'Insufficient balance in user wallet.';
+    END IF;
 ELSE
-    -- Throw an error if user has insufficient balance
-    RAISE EXCEPTION 'Insufficient balance in user wallet.';
+    -- Throw an error if user does not exist
+    RAISE EXCEPTION 'User does not exist.';
 END IF;
 
 COMMIT;
+
+
+
+
+
+
+
+
+
+
+
